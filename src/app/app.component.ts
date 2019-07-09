44e95c624fd0542af79ab2675d3e9f9c0dbbe98b
jpmanjunath <manjunathjp1992@gmail.com>
2019-07-09 10:06:58 +0530

    comitted for loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientService } from './service/http-client.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employee-management';
+
+
 }
